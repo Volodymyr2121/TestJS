1594c5e1cc7eadc3f6b43305cbc7a39a38fff09d
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2025-02-24 21:58:32 +0100

    numbers

--- javascript.js
+++ javascript.js
@@ -1,17 +1,36 @@
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function() { // показує поточний крок
-    alert(this.step);
-    return this;
-  }
-};
+// 1
+function readNumber() {
+  let num;
 
-ladder.up().up().down().showStep().down().up().showStep();
+  do {
+    num = prompt("Введіть число", 0);
+  } while ( !isFinite(num) );
+
+  if (num === null || num === '') return null;
+
+  return +num;
+}
+
+alert(`Read: ${readNumber()}`);
+
+
+// 2
+
+function random( max) {
+  return  Math.random() * max;
+}
+
+console.log( random(5) );
+console.log( random(5) );
+console.log(random(5));
+
+// 3
+
+function randomInteger(min, max) {
+  const num = min + Math.random() * (max-min)
+  return Math.round(num)
+}
+
+console.log( randomInteger(1, 10) );
+console.log( randomInteger(1, 10) ); 
+console.log( randomInteger(1, 10) ); 
