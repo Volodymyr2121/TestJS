4d5d3c748a2ef8c6f7315478ef20166086357302
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2025-03-05 21:25:44 +0100

    array metods

--- javascript.js
+++ javascript.js
@@ -1,57 +1,36 @@
 // 1
-const styles = ["Jazz", "Blues"];
-styles.push("Rock-n-Roll");
+function camelize(str) {
+  const arr = str.split("-");
+  const arrLowerCase = arr.map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1));
+  const result = arrLowerCase.join("")
+  
+  return console.log(result)
+};
 
-console.log(styles);
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
 
-const medians = Math.round((styles.length) / 2);
-styles[medians - 1] = "Classics";
+//2
+const filterRange = (arr, a, b) => {
+  const result = arr.filter(item => item >= a && item <= b)
+  
+  console.log(result)
+};
 
-console.log(styles);
-
-console.log(styles.shift());
-
-styles.unshift("Rap", "Reggae")
-console.log(styles)
-
-// 2
-
-function sumInput() {
-    let arr = [];
-    let sum = 0;
-   while (true) {
-       let userNum = prompt("Please enter number:");
-       
-       if(userNum === null || userNum.trim() === "" || isNaN(userNum)) break
-        arr.push(+userNum);
-   }
-
-    for (let element of arr) {
-     sum += element
-    }
-
-    return console.log(sum)
-}
-
-sumInput()
+filterRange([5, 3, 8, 1], 1, 4)
 
 // 3
-function getMaxSubSum(arr) {
-  let maxSum = 0;
-  let partialSum = 0;
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
 
-  for (let item of arr) { 
-    partialSum += item; 
-    maxSum = Math.max(maxSum, partialSum); 
-    if (partialSum < 0) partialSum = 0; 
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
   }
-
-  return maxSum;
+console.log(arr)
 }
 
-console.log( getMaxSubSum([-1, 2, 3, -9]) );
-console.log( getMaxSubSum([-1, 2, 3, -9, 11]) );
-console.log( getMaxSubSum([-2, -1, 1, 2]) );
-console.log( getMaxSubSum([100, -9, 2, -3, 5]) );
-console.log( getMaxSubSum([1, 2, 3]) );
-console.log( getMaxSubSum([-1, -2, -3]) ); 
+filterRangeInPlace([5, 3, 8, 1], 1, 4);
