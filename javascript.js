6c8f6f1756a44e72a29f45c576fd4ab2ea4fff90
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2025-03-11 20:46:11 +0100

    Map and Set

--- javascript.js
+++ javascript.js
@@ -1,82 +1,30 @@
-// 1
-function camelize(str) {
-  const arr = str.split("-");
-  const arrLowerCase = arr.map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1));
-  const result = arrLowerCase.join("")
-  
-  return console.log(result)
+//1
+function unique(arr) {
+  let newSet = new Set(arr);
+  return console.log(newSet);
 };
 
-camelize("background-color");
-camelize("list-style-image");
-camelize("-webkit-transition");
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+unique(values)
 
 //2
-const filterRange = (arr, a, b) => {
-  const result = arr.filter(item => item >= a && item <= b)
-  
-  console.log(result)
-};
 
-filterRange([5, 3, 8, 1], 1, 4)
+function aclean(arr) {
+  let obj = {};
 
-// 3
-function filterRangeInPlace(arr, a, b) {
-    for (let i = 0; i < arr.length; i++) {
-    let val = arr[i];
-
-    if (val < a || val > b) {
-      arr.splice(i, 1);
-      i--;
-    }
+  for (let i = 0; i < arr.length; i++) {
+    let sorted = arr[i].toLowerCase().split("").sort().join("");
+    obj[sorted] = arr[i];
   }
-console.log(arr)
+
+  return Object.values(obj);
 }
 
-filterRangeInPlace([5, 3, 8, 1], 1, 4);
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
-//4
-function copySorted(arr) {
-  const result = arr.slice().sort()
-  return console.log(result)
-}
+alert(aclean(arr));
 
-copySorted(["HTML", "JavaScript", "CSS"])
-
-//5
-
-const userArray = [{ name: "Іван", age: 25 },
-  { name: "Петро", age: 30 },
-  { name: "Марія", age: 28 }
-]
-
-function getNameArray() {
-  const result = userArray.map(user => user.name);
-
-return console.log(result)
-}
-getNameArray();
-
-//6
-const userArr = [{ name: "Іван", surname: "Іванко", id: 1 },
-  { name: "Петро", surname: "Петренко", id: 2 },
-  { name: "Марія", surname: "Мрійко", id: 3 }
-]
-
-
-
-  const result= userArr.map(item => ({
-    fullName: `${item.name} ${item.surname}`,
-    id: item.id,
-  }))
-
-console.log(result)
-
-//7
-const children = [{ name: "John", age: 25 },{ name: "Pete", age: 30 },{ name: "Mary", age: 29 }];
-
-const arr = children.reduce((prev, user) => prev + user.age, 0) / children.length;
-
-console.log(arr)
-
-//8
+//3
